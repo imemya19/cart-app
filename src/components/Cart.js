70df68dbee2854302d8eeb1268b38fd6add2fd6a
imemya19 <somyaagrawal.ime19@gmail.com>
2021-08-15 21:47:48 +0530

    added fns through props

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,12 +3,78 @@ import CartItem from './CartItem'
 
 
 class Cart extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            products:[
+                {
+                    title:"Choclate Box",
+                    price:500,
+                    qty:2
+                },
+                {
+                    title:"Uncle chips",
+                    price:200,
+                    qty:18
+                },
+                {
+                    title:"Fried momos",
+                    price:350,
+                    qty:6
+                }
+            ]     
+      }
+    }
+
+    increaseQty = (item)=> {
+        const {products} =this.state;
+        const index = products.indexOf(item);
+        products[index].qty +=1;
+
+        this.setState({
+            products
+        })
+      }
+      
+      decreaseQty = (item)=> {
+        const {products} =this.state;
+        const index = products.indexOf(item);
+        if(products[index].qty ===0) return;
+
+        products[index].qty -=1;
+        this.setState({
+            products
+        })
+      }
+
+      deleteItem = (item)=>{
+        const {products} =this.state;
+        const index = products.indexOf(item);
+        const items = products.filter((item)=>item!==products[index])
+
+        this.setState({
+            products:items
+        })
+      }
+
 render(){
+    const {products} = this.state;
     return (
       <div className="cart">
-    <h1>Our Cart-App</h1>
-      <CartItem title="somya"/>
-      <CartItem title="shivang"/>
+        <h1>Our Cart-App</h1>
+
+
+      {products.map((item)=>{
+          return(
+            <CartItem 
+            item={item} 
+            handleIncQty = {this.increaseQty}
+            handleDecQty = {this.decreaseQty}
+            handleDelItem = {this.deleteItem}
+            />
+      )
+ 
+      })}
 
       </div>
     );
--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -3,33 +3,11 @@ import {FaPlusCircle,FaMinusCircle,FaTrash} from 'react-icons/fa'
 import React from 'react'
 
 class CartItem extends React.Component{
-constructor(){
-  super();
-  this.state={
-    title:"Choclate Box",
-    price:500,
-    qty:2
-  }
-}
-increaseQty = ()=> {
-  this.setState((prevState)=>{
-    return{
-      qty:prevState.qty+1
-    }
-  })
-}
 
-deccreaseQty = ()=> {
-  this.setState((prevState)=>{
-    if(prevState.qty==1) return;
-    return{
-      qty:prevState.qty-1
-    }
-  })
-}
+
   render(){
-const {title} = this.props ;
-   const {price,qty} = this.state;
+   const {title,price,qty} = this.props.item;
+   const {item, handleIncQty,handleDecQty,handleDelItem} =this.props;
     return (
       <div className="cart-item">
         <div className="item-image">img</div>
@@ -38,9 +16,9 @@ const {title} = this.props ;
           <div> Rs. <span>{price}</span> </div>
           <div>Qty : <span>{qty}</span></div>
           <div className="action-buttons">
-            <FaPlusCircle className="action" onClick={this.increaseQty}/>
-            <FaMinusCircle className="action" onClick={this.deccreaseQty}/>
-            <FaTrash className="action"/> 
+            <FaPlusCircle className="action" onClick={()=>handleIncQty(item)}/>
+            <FaMinusCircle className="action" onClick={()=>handleDecQty(item)}/>
+            <FaTrash className="action" onClick={()=>handleDelItem(item)}/> 
           </div>
         </div>
       </div>
